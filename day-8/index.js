991b828e81ba7f7e5ef45f530e0078ad936dd0da
Bhaskar Das <bhaskar6493@gmail.com>
2026-07-15 18:29:59 +0530

    Frontend development

--- day-8/index.js
+++ day-8/index.js
@@ -1,6 +1,5 @@
 //let, const, var
 let name = "bhaskar";
-
 console.log(name);
 
 let age=22;
@@ -18,3 +17,18 @@ console.log(typeof age);
 let firstName="bhaskar Jyoti";
 let lastName="Das";
 console.log(firstName,lastName);
+
+let num2=40;
+let num3=40;
+console.log(num2<num3);
+
+//if else statement
+if (num2 > num3) {
+    console.log("i am bhaskar");
+} else if (num2 < num3) {
+    console.log("i am not bhaskar");
+}else{
+    console.log("i am bhaskar jyoti Das");
+}
+
+
