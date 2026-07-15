f7035dc77887a2b812c5530c7d71b2603798b613
Bhaskar Das <bhaskar6493@gmail.com>
2026-07-15 18:02:14 +0530

    Java script

---
+++ day-8/index.js
@@ -0,0 +1,20 @@
+//let, const, var
+let name = "bhaskar";
+
+console.log(name);
+
+let age=22;
+console.log(age);
+
+let num=10;
+console.log(num);
+let num1=20;
+console.log(num1);
+let sum=num+num1;
+console.log(sum);
+
+console.log(typeof age);
+
+let firstName="bhaskar Jyoti";
+let lastName="Das";
+console.log(firstName,lastName);
